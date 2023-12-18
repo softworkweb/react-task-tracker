59278b6c3ef37ea32dcb1c6b16d7becea3b4dd1a
Egbe Raymond <softworkweb@gmail.com>
2023-12-18 01:25:21 +0100

    fixing the checkbox to return unchecked after save

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import Header from './components/Header';
-import AddButton from './components/AddButton';
-import CancelButton from './components/CancelButton';
-import RemoveBtn from './components/Buttons/RemoveBtn';
+import AddButton from './components/Buttons/AddButton';
+import CancelButton from './components/Buttons/CancelButton';
 import TasksList from './components/TasksList';
 import NewTask from './components/NewTask';
 function App() {
@@ -22,61 +21,58 @@ function App() {
     });
   };
   const taskItems = [...newTasks];
-
+  console.log(taskItems.length);
   return (
     <>
       <div className="mx-auto flex justify-center  h-screen text-[18px] text-[#0e181d] relative md:text-[22px]">
-        <div className=" flex flex-col bg-[#ECFEFF] h-sceen item-center ">
+        <div className=" flex flex-col item-center ">
           <div className="">
             <Header />
           </div>
-          <div className=" flex justify-center my-[2vh]">
-            {addBtn ? (
-              <AddButton addBtn={addBtn} setAddBtn={setAddBtn} />
-            ) : (
-              <>
-                <div>
-                  <CancelButton addBtn={addBtn} setAddBtn={setAddBtn} />
-                  <div className="my-[2vh]">
-                    <NewTask
-                      task={task}
-                      setTask={setTask}
-                      dayTime={dayTime}
-                      setDayTime={setDayTime}
-                      check={check}
-                      setCheck={setCheck}
-                      updatedTaskList={updatedTaskList}
-                    />
-                  </div>
-                </div>
-              </>
-            )}
-          </div>
-          {/* {taskItems.map((taskItem) => (
+          <div className=" bg-[#ECFEFF] h-screen ">
             <div className=" flex justify-center my-[2vh]">
-              <h1>{taskItem.task}</h1>
-              <p>{taskItem.day_Time}</p>
-              {taskItem.checked ? <p>true</p> : <p>false</p>}
-            </div>
-          ))} */}
-          <div className="my-[50px] flex flex-col md:my-[150px]">
-            <div className="flex items-center w-full my-[1px] md:my-[5px]">
-              <hr className="w-[5px] h-full bg-success"></hr>
-              <div className="w-full">
-                <hr className="bg-[#0b141820] h-[2px]"></hr>
-                <div className="flex items-center justify-between pr-3 md:pr-10">
-                  <div className="p-3 md:p-5">
-                    <h1 className="font-bold">Pick up trash</h1>
-                    <p className="text-[16px] text-[#0b141868] md:text-[18px]">
-                      Wednesday, 8am
-                    </p>
-                  </div>
+              {addBtn ? (
+                <AddButton addBtn={addBtn} setAddBtn={setAddBtn} />
+              ) : (
+                <>
                   <div>
-                    <RemoveBtn />
+                    <CancelButton addBtn={addBtn} setAddBtn={setAddBtn} />
+                    <div className="my-[2vh]">
+                      <NewTask
+                        task={task}
+                        setTask={setTask}
+                        dayTime={dayTime}
+                        setDayTime={setDayTime}
+                        check={check}
+                        setCheck={setCheck}
+                        updatedTaskList={updatedTaskList}
+                      />
+                    </div>
+                  </div>
+                </>
+              )}
+            </div>
+            <div className="my-[50px] bg-[#ECFEFF] flex flex-col md:my-[150px]">
+              {taskItems.length > 0 ? (
+                taskItems.map((taskItem) => (
+                  <TasksList
+                    task={taskItem.task}
+                    day_Time={taskItem.day_Time}
+                    checked={taskItem.checked}
+                  />
+                ))
+              ) : (
+                <div className=" w-full">
+                  <hr className="bg-[#0b141820] h-[2px] w-full"></hr>
+                  <div className="flex justify-center pr-3 md:pr-10">
+                    <div className="p-3 md:p-5">
+                      <p className="text-[16px] text-[#0b141868] md:text-[18px]">
+                        Your tasklist empty
+                      </p>
+                    </div>
                   </div>
                 </div>
-                <hr className="bg-[#0b141820] text-danger h-[2px]"></hr>
-              </div>
+              )}
             </div>
           </div>
         </div>
--- src/components/TasksList.js
+++ src/components/TasksList.js
@@ -1,11 +1,34 @@
-import React from 'react'
+import React from 'react';
+import RemoveBtn from './Buttons/RemoveBtn';
 
-function TasksList() {
+function TasksList({ task, day_Time, checked }) {
   return (
-    <div>
-      hxkc
-    </div>
-  )
+    <>
+      <div className="flex items-center w-full my-[1px] md:my-[5px]">
+        {checked ? (
+          <hr className="w-[5px] h-full bg-success"></hr>
+        ) : (
+          <hr className="w-[5px] h-full bg-success/1"></hr>
+        )}
+
+        <div className="w-full">
+          <hr className="bg-[#0b141820] h-[2px]"></hr>
+          <div className="flex items-center justify-between pr-3 md:pr-10">
+            <div className="p-3 md:p-5">
+              <h1 className="font-bold">{task}</h1>
+              <p className="text-[16px] text-[#0b141868] md:text-[18px]">
+                {day_Time}
+              </p>
+            </div>
+            <div>
+              <RemoveBtn />
+            </div>
+          </div>
+          <hr className="bg-[#0b141820] text-danger h-[2px]"></hr>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default TasksList
+export default TasksList;
